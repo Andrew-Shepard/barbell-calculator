393da0910ba6fdf6d39e38af0863924173aa3bd4
Andrew-Shepard <andrewshepard@outlook.com>
2023-07-18 14:23:40 -0400

    Inital Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
+import { Form, Button, ListGroup } from 'react-bootstrap';
 
-function App() {
+const App = () => {
+  const [weight, setWeight] = useState(0);
+  const [plates, setPlates] = useState([]);
+  const [remainingWeight, setRemainingWeight] = useState(0);
+
+  const calculatePlates = () => {
+    const barbellWeight = 45;
+    const availablePlates = [2.5, 5, 10, 25, 35, 45];
+    const totalWeight = parseFloat(weight);
+    
+    // Check if the total weight is less than the base weight (45)
+    if (totalWeight < barbellWeight) {
+      setPlates([]); // Do not add any plates
+      setRemainingWeight(totalWeight);
+      return;
+    }
+    
+    const targetWeight = totalWeight - barbellWeight;
+    let remainingWeight = targetWeight;
+  
+    const calculatedPlates = [];
+  
+    // Loop through the available plates from highest to lowest weight
+    for (let i = availablePlates.length - 1; i >= 0; i--) {
+      const plateWeight = availablePlates[i];
+      const plateCount = Math.floor(remainingWeight / (plateWeight * 2));
+  
+      // If the plateCount is greater than 0, add the plate pair to the barbell
+      if (plateCount > 0) {
+        calculatedPlates.push({ weight: plateWeight, count: plateCount });
+        remainingWeight -= plateWeight * 2 * plateCount;
+        if (remainingWeight === 0) {
+          break;
+        }
+      }
+    }
+  
+    // Handle the remaining weight if it cannot be perfectly balanced
+    if (remainingWeight > 0) {
+      // Add the smallest available plate pair to accommodate the remaining weight
+      const smallestPlate = Math.min(...availablePlates);
+      const remainingPlateCount = Math.ceil(remainingWeight / (smallestPlate * 2));
+      calculatedPlates.push({ weight: smallestPlate, count: remainingPlateCount });
+      remainingWeight -= smallestPlate * 2 * remainingPlateCount;
+    }
+  
+    // Duplicate the calculated plates for both sides of the barbell
+    const balancedPlates = [...calculatedPlates.reverse(), ...calculatedPlates.reverse()];
+  
+    setPlates(balancedPlates);
+    setRemainingWeight(remainingWeight); // Display the remaining weight and plates added
+  };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Plate Calculator</h1>
+      <Form>
+        <Form.Group controlId="weightInput">
+          <Form.Label>Barbell Weight (in lbs):</Form.Label>
+          <Form.Control
+            type="number"
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+            placeholder="Enter weight"
+          />
+        </Form.Group>
+        <Button variant="primary" onClick={calculatePlates} block>
+          Calculate Plates
+        </Button>
+      </Form>
+      {plates.length > 0 && (
+        <div>
+          <h2>Plates Required:</h2>
+          <div className="barbell-container">
+            <div className="left-side">
+              <h3>Left Side:</h3>
+              <ListGroup>
+                {plates.slice(0, Math.ceil(plates.length / 2)).map((plate, index) => (
+                  <ListGroup.Item key={index}>
+                    {plate.count} x {plate.weight} lb plates
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </div>
+            <hr className="bar" />
+            <div className="right-side">
+              <h3>Right Side:</h3>
+              <ListGroup>
+                {plates.slice(Math.ceil(plates.length / 2)).map((plate, index) => (
+                  <ListGroup.Item key={index}>
+                    {plate.count} x {plate.weight} lb plates
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </div>
+          </div>
+        </div>
+      )}
+
+
+    {remainingWeight !== 0 && (<h2>Remaining Weight: {remainingWeight} lbs</h2>)}
     </div>
   );
-}
+};
 
 export default App;
+
