9ac4ef8ba8874a0ed2c1226d90ab61b1d8b7a7c4
Andrew-Shepard <andrewshepard@outlook.com>
2023-07-19 16:34:14 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -8,16 +8,30 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', (event) => {
-  // Perform installation steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
+    // Perform installation steps
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then((cache) => {
+          console.log('Opened cache');
+          
+          let cachePromises = urlsToCache.map(urlToCache => {
+            return fetch(urlToCache)
+              .then(response => {
+                if (!response.ok) {
+                  throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return cache.put(urlToCache, response);
+              })
+              .catch(error => {
+                console.error(`Failed to fetch and cache ${urlToCache}`, error);
+              });
+          });
+  
+          return Promise.all(cachePromises);
+        })
+    );
+  });
+  
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
