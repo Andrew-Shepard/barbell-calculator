f1058e198a257ac44928fb2bedfa0bf4e4d53bbc
Andrew-Shepard <andrewshepard@outlook.com>
2023-07-19 14:25:45 -0400

    Updates

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+// service-worker.js
+
+const CACHE_NAME = 'my-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/barbell.png'
+];
+
+self.addEventListener('install', (event) => {
+  // Perform installation steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return the response
+        if (response) {
+          return response;
+        }
+        // Otherwise, fetch from the network
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  // Clean up old caches
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
